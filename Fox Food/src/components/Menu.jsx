3a000aa88ba13a900ee18ca677dce72edc2e89a1
sufyanhabib <sufyanhabib80@gmail.com>
2025-08-15 12:20:15 +0530

    Minor changes- some style changes on (.navbar ul li a display:block) and some Menu.js

--- Fox Food/src/components/Menu.jsx
+++ Fox Food/src/components/Menu.jsx
@@ -1,8 +1,12 @@
-
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div>
+      <h1>Menu</h1>
+      <h2></h2>
+      <h3></h3>
+      <p></p>
+    </div>
   )
 }
 
-export default Menu
+export default Menu;
